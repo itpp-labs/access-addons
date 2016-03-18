2b206a7c50027cc8d7621504bb62154f69fcb57c
Ivan Yelizariev <yelizariev@it-projects.info>
2016-03-18 13:19:14 +0500

    hidden_admin: add mail to dependency

--- hidden_admin/__openerp__.py
+++ hidden_admin/__openerp__.py
@@ -4,7 +4,10 @@
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'category': 'Tools',
     'website': 'https://twitter.com/yelizariev',
-    'depends': ['ir_rule_protected'],
+    'depends': [
+        'mail', # we need this dependency to avoid test errors on --test-enabled
+        'ir_rule_protected',
+    ],
     'data': [
         'security.xml',
     ],
