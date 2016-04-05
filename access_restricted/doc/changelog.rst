930c20e7b34efd5b93690ab68948aa032aeaaf1b
Ivan Yelizariev <yelizariev@it-projects.info>
2016-04-05 20:38:43 +0500

    [DOC] new version 1.1.0
    [IMP] Make restricted groups readonly in Settigs pages (res.config.settings)

---
+++ access_restricted/doc/changelog.rst
@@ -0,0 +1,13 @@
+Changelog
+=========
+
+`1.1.0`
+-------
+
+- ADD: Make restricted groups readonly in Settigs pages (res.config.settings)
+- ADD: don't restrict access to Technical Settings group
+
+`1.0.0`
+-------
+
+- init version
--- access_restricted/models.py
+++ access_restricted/models.py
@@ -1,5 +1,7 @@
 from openerp import models, api, exceptions, SUPERUSER_ID
 from openerp.addons.base.res.res_users import is_reified_group
+from openerp.tools.translate import _
+from openerp.tools import ustr
 
 IR_CONFIG_NAME = 'access_restricted.fields_view_get_uid'
 
@@ -52,3 +54,40 @@ class ResGroups(models.Model):
             _model, group_no_one_id = model_data_obj.get_object_reference(cr, uid, 'base', 'group_no_one')
             domain = domain + ['|', ('users', 'in', [uid]), ('id', '=', group_no_one_id)]
         return self.search(cr, uid, domain, context=context)
+
+
+class res_config_settings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    def _get_classified_fields(self, cr, uid, context=None):
+        classified = super(res_config_settings, self)._get_classified_fields(cr, uid, context=context)
+        if uid == SUPERUSER_ID:
+            return classified
+
+        group = []
+        for name, groups, implied_group in classified['group']:
+            if self.pool['res.users'].search_count(cr, uid, [('id', '=', uid), ('groups_id', 'in', [implied_group.id])]):
+                group.append((name, groups, implied_group))
+        classified['group'] = group
+        return classified
+
+
+    def fields_get(self, cr, uid, fields=None, context=None, write_access=True, attributes=None):
+        fields = super(res_config_settings, self).fields_get(
+            cr, uid, fields, context, write_access, attributes)
+
+        if uid == SUPERUSER_ID:
+            return fields
+
+        for name in fields:
+            if not name.startswith('group_'):
+                continue
+            f = self._columns[name]
+            if self.pool['res.users'].has_group(cr, uid, f.implied_group):
+                continue
+
+            fields[name].update(
+                readonly=True,
+                help= ustr(fields[name].get('help', '')) +
+                     _('\n\nYou don\'t have access to change this settings, because you administration rights are restricted'))
+        return fields
