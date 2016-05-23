95dec59b409f951b2abfc6b73d288f715d191493
ufaks <excellm@yandex.ru>
2016-05-23 18:29:54 +0500

    [FIX] add pre_init_hook

--- res_users_clear_access_rights/__init__.py
+++ res_users_clear_access_rights/__init__.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import models
+
+from openerp import SUPERUSER_ID
+from collections import OrderedDict
+from openerp.tools import float_compare
+
+def pre_init_hook(cr, registry):
+    # It is necessary for compatibility with the module "access_restricted"
+    registry['res.groups'].update_user_groups_view(cr, SUPERUSER_ID)
+    return
--- res_users_clear_access_rights/__openerp__.py
+++ res_users_clear_access_rights/__openerp__.py
@@ -15,4 +15,5 @@
     ],
     'installable': True,
     'auto_install': False,
+    'pre_init_hook': 'pre_init_hook',
 }
