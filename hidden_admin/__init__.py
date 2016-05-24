5ee3cbfe58b60e77516c1d6bbdbb1858ce6a01e0
Ivan Yelizariev <yelizariev@it-projects.info>
2016-05-24 15:53:12 +0500

    [FIX] import

--- hidden_admin/__init__.py
+++ hidden_admin/__init__.py
@@ -1,5 +1,5 @@
 def post_load():
     """Delete tests, that check behavior changed by this module"""
-    from openerp.addons.mail.tests import TestMailMessage
+    from openerp.addons.mail.tests.test_mail_message import TestMailMessage
     del TestMailMessage.test_50_mail_flow_access_rights
     del TestMailMessage.test_40_message_vote
