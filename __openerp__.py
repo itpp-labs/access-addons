357712bdc7fb8019f9f353b9c4e96198561dbb1a
x620 <romanchenko@it-projects.info>
2016-07-08 12:37:38 +0500

    [ADD] Create module access_limit_records_number
    [DOC] changelog.rst
    
    [DOC] Added index.rst
    
    [DOC] Added README.rst
    
    [ADD] Created base.limit.records_number model
    
    [ADD] Added menu item, tree, form and search for base.limit.records_number
    
    [ADD] Added methods for verify records number

---
+++ __openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Limit for tables""",
+    "summary": """Module allow limit records of the table""",
+    "category": "Extra tools",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "IT-Projects LLC, Pavel Romanchenko",
+    "website": "https://it-projects.info",
+    "license": "LGPL-3",
+    #"price": 9.00,
+    #"currency": "EUR",
+
+    "depends": [
+        'base_action_rule',
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        'views/base_limit.xml',
+        'data/data.xml',
+    ],
+    "qweb": [],
+    "demo": [],
+
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "installable": True,
+    "auto_install": False,
+}
