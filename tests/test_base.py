4bdd4e698caa4e41b5ba85166d82cbf420deb9ca
Ivan Yelizariev <yelizariev@it-projects.info>
2016-08-19 20:10:50 +0500

    [REF] fix lint errors (9.0)

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,7 @@
 from openerp.tests import common
 import json
 
+
 @common.at_install(False)
 @common.post_install(True)
 class test_base(common.TransactionCase):
@@ -9,8 +10,8 @@ class test_base(common.TransactionCase):
         demo_user = self.env.ref('base.user_demo')
 
         # ok
-        self.env['base.limit.records_number.test'].sudo(demo_user).create({'name':'r1'})
+        self.env['base.limit.records_number.test'].sudo(demo_user).create({'name': 'r1'})
 
         # limit 1 is reached
         with self.assertRaises(Exception):
-            self.env['base.limit.records_number.test'].sudo(demo_user).create({'name':'r2'})
+            self.env['base.limit.records_number.test'].sudo(demo_user).create({'name': 'r2'})
