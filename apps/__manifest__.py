6d05f11275c0d80762c3e7951f25fa39fd796418
Dinar <gabbasov@it-projects.info>
2016-11-10 16:47:27 +0500

    [ADD] apps (#153)

---
+++ apps/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Apps',
+    'version': '1.0.0',
+    'author': 'IT-Projects LLC, Dinar Gabbasov',
+    'category': 'Access',
+    'website': 'https://twitter.com/gabbasov_dinar',
+    'license': 'GPL-3',
+    'depends': [
+        'access_apps',
+    ],
+    'data': [
+        'views/apps_view.xml',
+        'security.xml',
+    ],
+    'installable': True
+}
