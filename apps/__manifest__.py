62aaff838db681b1402c19980ec770281082f654
Ivan Yelizariev <yelizariev@it-projects.info>
2016-11-17 15:49:39 +0500

    [ADD] uninstall dependencies of super module (#156)
    [IMP] make prefix customizable
    [DOC] add doc/index.rst

--- apps/__manifest__.py
+++ apps/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Apps',
-    'summary': """ """,
+    'name': 'Custom Apps',
+    'summary': """Simplify Apps Interface""",
     'images': [],
     'version': '1.0.0',
     'application': False,
     'author': 'IT-Projects LLC, Dinar Gabbasov',
     'website': 'https://twitter.com/gabbasov_dinar',
     'category': 'Access',
-    'license': 'GPL-3',
+    'license': 'LGPL-3',
 
     'depends': [
         'access_apps',
@@ -16,6 +16,7 @@
     'data': [
         'views/apps_view.xml',
         'security.xml',
+        'data/ir_config_parameter.xml',
     ],
 
     'post_load': None,
---
+++ apps/models/ir_config_parameter.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api
+
+KEY = 'apps.super_app_prefix'
+
+
+class IrConfigParameter(models.Model):
+    _inherit = 'ir.config_parameter'
+
+    @api.model
+    def _recompute_is_super_app(self):
+        self.env['ir.module.module'].search([])._compute_is_super_app()
+
+    @api.model
+    def create(self, vals):
+        res = super(IrConfigParameter, self).create(vals)
+        if vals and vals.get('key') == KEY:
+            self._recompute_is_super_app()
+        return res
+
+    @api.multi
+    def write(self, vals):
+        res = super(IrConfigParameter, self).write(vals)
+        if self.key == KEY:
+            self._recompute_is_super_app()
+        return res
