71c98e1075ff0341998849594a2345d2d5e43385
ILMIR <karamov@it-projects.info>
2016-11-17 14:26:16 +0500

    [DOC] 'apps' documentation  (#155)
    * [DOC] apps module
    
    * [DOC] return twitter back

--- apps/__manifest__.py
+++ apps/__manifest__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Apps',
+    'summary': """ """,
+    'images': [],
     'version': '1.0.0',
+    'application': False,
     'author': 'IT-Projects LLC, Dinar Gabbasov',
-    'category': 'Access',
     'website': 'https://twitter.com/gabbasov_dinar',
+    'category': 'Access',
     'license': 'GPL-3',
+
     'depends': [
         'access_apps',
     ],
@@ -13,5 +17,11 @@
         'views/apps_view.xml',
         'security.xml',
     ],
+
+    'post_load': None,
+    'pre_init_hook': None,
+    'post_init_hook': None,
+
+    'auto_install': False,
     'installable': True
 }
