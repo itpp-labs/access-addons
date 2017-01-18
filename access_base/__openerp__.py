c41b809ebf90cde597056aefba6f07d7d6a59a28
Ivan Yelizariev <yelizariev@it-projects.info>
2017-01-18 17:48:00 +0500

    [DOC] add support email

--- access_base/__openerp__.py
+++ access_base/__openerp__.py
@@ -5,6 +5,7 @@
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     'license': 'LGPL-3',
     "category": "Access",
+    "support": "apps@it-projects.info",
     'website': 'https://yelizariev.github.io',
     'images': ['images/user.png'],
     'depends': ['base', 'res_users_clear_access_rights'],
--- access_restricted/__openerp__.py
+++ access_restricted/__openerp__.py
@@ -4,6 +4,7 @@
     'version': '1.2.0',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     "category": "Access",
+    "support": "apps@it-projects.info",
     'website': 'https://twitter.com/yelizariev',
     "license": "LGPL-3",
     'price': 30.00,
