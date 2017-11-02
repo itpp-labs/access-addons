31a8dc7aaba1c69045f41ac38956ffe10e2b5c34
Ildar Nasyrov <nasyrov@it-projects.info>
2017-11-02 04:24:35 +0000

    [IMP] access_apps: avoid copy-pasting

--- access_apps/models/res_config.py
+++ access_apps/models/res_config.py
@@ -8,8 +8,6 @@ class ResConfigSettings(models.TransientModel):
     @api.model
     def _install_modules(self, modules):
         if self.env['res.users'].has_group('access_apps.group_allow_apps_only_from_settings'):
-            result = super(ResConfigSettings, self.sudo())._install_modules(modules)
-        else:
-            result = super(ResConfigSettings, self)._install_modules(modules)
+            self = self.sudo()
 
-        return result
+        return super(ResConfigSettings, self)._install_modules(modules)
