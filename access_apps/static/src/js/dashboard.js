f51696ce842f9f2ad90d5e1667f23a0faeb29b93
Ildar Nasyrov <nasyrov@it-projects.info>
2017-11-10 13:07:47 +0000

    [REM] access_apps: bad solution

--- access_apps/static/src/js/dashboard.js
+++ access_apps/static/src/js/dashboard.js
@@ -4,23 +4,22 @@ odoo.define('access_apps.dashboard', function (require) {
     var dashboard = require('web_settings_dashboard');
     var Model = require('web.Model');
 
+    var def = $.Deferred();
+
     dashboard.Dashboard.include({
 
-        willStart:function(){
-            var self = this;
-	    var Users = new Model('res.users');
-		return Users.call('has_group', ['access_apps.group_allow_apps']).then(function (result) {
-			self.has_access_to_apps = result;
-		});
+        start: function(){
+	    return this._super().then(function() {
+		def.resolve();
+	    });
         },
 
-        start: function(){
-            if(!this.has_access_to_apps) {
-		this.all_dashboards = _.without(this.all_dashboards, 'apps');
-                this.$('.o_web_settings_dashboard_apps').parent().remove();
-	    }
-            return this._super();
+        load_apps: function(data){
+            if(data.has_access_to_apps) {
+                return this._super(data);
+            }
+            this.$('.o_web_settings_dashboard_apps').parent().remove();
         }
-
-   });
+    });
+    return {'ready': def};
 });
