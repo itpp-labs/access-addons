dac4c672e1a701db63ed29e2f793edd17da579ed
Ildar Nasyrov <nasyrov@it-projects.info>
2017-11-10 08:29:26 +0000

    Revert "[IMP] access_apps: optimize dashboard js - no need to load_apps if you have no rights to see the apps widget"
    This reverts commit d6c67d1dcd067a0773ba6422fb67dd6225aa0585.

--- access_apps/static/src/js/dashboard.js
+++ access_apps/static/src/js/dashboard.js
@@ -4,23 +4,11 @@ odoo.define('access_apps.dashboard', function (require) {
     var dashboard = require('web_settings_dashboard');
 
     dashboard.Dashboard.include({
-
-        init: function(parent, data){
-            var ret = this._super(parent, data);
-	    this.has_access_to_apps = data.has_access_to_apps;
-            if(!this.has_access_to_apps) {
-                this.all_dashboards = _.without(this.all_dashboards, 'apps');
-                this.$('.o_web_settings_dashboard_apps').parent().remove();
-	    }
-            return ret;
-        },
-
-        start: function(){
-            if(!this.has_access_to_apps) {
-                this.$('.o_web_settings_dashboard_apps').parent().remove();
-	    }
-        return this._super();
+        load_apps: function(data){
+            if(data.has_access_to_apps) {
+                return this._super(data);
+            }
+            this.$('.o_web_settings_dashboard_apps').parent().remove();
         }
-
    });
 });
