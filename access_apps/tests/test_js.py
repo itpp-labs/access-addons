f75389a546a61379c2dafc09e14378b3476e53b2
Ildar Nasyrov <nasyrov@it-projects.info>
2017-11-13 06:55:31 +0000

    [FIX] access_apps: do not use self.env in tests

--- access_apps/tests/test_js.py
+++ access_apps/tests/test_js.py
@@ -7,9 +7,10 @@ import odoo.tests
 class TestUi(odoo.tests.HttpCase):
 
     def test_01_dashboard_remove(self):
-        demo_user = self.env.ref('base.user_demo')
-        system_group = self.env.ref('base.group_system')
-        allow_apps_group = self.env.ref('access_apps.group_allow_apps')
+        phantom_env = api.Environment(self.registry.test_cr, self.uid, {})
+        demo_user = phantom_env.ref('base.user_demo')
+        system_group = phantom_env.ref('base.group_system')
+        allow_apps_group = phantom_env.ref('access_apps.group_allow_apps')
         demo_user.write({'groups_id': [(4, system_group.id)]})
         demo_user.write({'groups_id': [(3, allow_apps_group.id)]})
         self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('removed_apps_dashboard')", "odoo.__DEBUG__.services['access_apps.dashboard'].ready.state() == 'resolved'", login="demo")
