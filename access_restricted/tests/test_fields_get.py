b61de951f6275441a3b8327d24dfa36778165354
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-04 11:42:05 +0000

    [CI] access_restricted

--- access_restricted/tests/test_fields_get.py
+++ access_restricted/tests/test_fields_get.py
@@ -13,14 +13,19 @@ class TestFieldsGet(TransactionCase):
         group_system = self.env.ref('base.group_system')
 
         demo_user.write({'groups_id': [(3, group_system.id)]})
+        group_system.write({'users': [(3, demo_user.id)]})
         demo_user.write({'groups_id': [(4, group_erp_manager.id)]})
-        self.env['res.groups'].sudo()._update_user_groups_view()
+
+        self.env['res.groups'].sudo(demo_user.id)._update_user_groups_view()
+
         sel_groups = name_selection_groups([group_erp_manager.id])
         res = self.env['res.users'].sudo(demo_user).fields_get()
         self.assertTrue(res.get(sel_groups))
 
         demo_user.write({'groups_id': [(4, group_system.id)]})
-        self.env['res.groups'].sudo()._update_user_groups_view()
+
+        self.env['res.groups'].sudo(demo_user.id)._update_user_groups_view()
+
         sel_groups = name_selection_groups([group_erp_manager.id, group_system.id])
         res = self.env['res.users'].sudo(demo_user).fields_get()
         self.assertTrue(res.get(sel_groups))
