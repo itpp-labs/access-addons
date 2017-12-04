58364009bfcdafa339b24a5bed4f446817ed9576
Ildar Nasyrov <nasyrov@it-projects.info>
2017-12-04 12:21:08 +0000

    [CI] access_restricted

--- access_restricted/tests/test_fields_get.py
+++ access_restricted/tests/test_fields_get.py
@@ -16,15 +16,13 @@ class TestFieldsGet(TransactionCase):
         group_system.write({'users': [(3, demo_user.id)]})
         demo_user.write({'groups_id': [(4, group_erp_manager.id)]})
 
-        self.env['res.groups'].sudo(demo_user.id)._update_user_groups_view()
+        res = self.env['res.users'].sudo(demo_user).with_context({'uid': demo_user.id, 'search_default_no_share': 1, 'params': {'action': 71}}).load_views([[153, "list"], [152, "form"], [154, "search"]])
 
         sel_groups = name_selection_groups([group_erp_manager.id])
         res = self.env['res.users'].sudo(demo_user).fields_get()
         self.assertTrue(res.get(sel_groups))
 
         demo_user.write({'groups_id': [(4, group_system.id)]})
-
-        self.env['res.groups'].sudo(demo_user.id)._update_user_groups_view()
 
         sel_groups = name_selection_groups([group_erp_manager.id, group_system.id])
         res = self.env['res.users'].sudo(demo_user).fields_get()
