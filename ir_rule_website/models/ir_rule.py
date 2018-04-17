9d86e487115d4d4bac4bb5bb1a830450ba24d640
Ildar Nasyrov <nasyrov@it-projects.info>
2018-04-17 04:49:27 +0000

    [IMP] ir_rule_website: fields nameming

--- ir_rule_website/models/ir_rule.py
+++ ir_rule_website/models/ir_rule.py
@@ -2,27 +2,26 @@
 
 from odoo import api, models, tools, fields
 from odoo.addons.base.ir.ir_rule import IrRule as IrRuleOriginal
-from odoo.osv import expression
-from odoo.tools.safe_eval import safe_eval
 
 
 class IrRule(models.Model):
     _inherit = 'ir.rule'
 
-    backend_replacement = fields.Selection([
-        ("[(1, '=', 1)]", "[(1, '=', 1)]"),
-        ("[(0, '=', 1)]", "[(0, '=', 1)]"),
-    ], string='Replacement of multi-website rule definition for backend',
-        help="""Replacement for multi-website rule when getting access from backend. Leave empty for all other rules.
-        (there is usually no 'website_id' value in the rule evaluation context, when using backend).""")
+    backend_behaviour = fields.Selection([
+        ("true", "give access"),
+        ("false", "restrict access"),
+    ], string='Backend behaviour',
+        help="""This is bypass for main rule definition.
+        When working from backend there is usually no 'website_id' value in the rule evaluation context
+        what leads to SQL syntax error such as 'WHERE website_id IN ()' in rules that using 'website_id'""")
 
     @api.depends('domain_force')
     def _force_domain(self):
         eval_context = self._eval_context()
         if not eval_context.get('website_id'):
-            website_rules = self.filtered(lambda r: r.backend_replacement)
+            website_rules = self.filtered(lambda r: r.backend_behaviour)
             for rule in website_rules:
-                rule.domain = expression.normalize_domain(safe_eval(rule.backend_replacement, eval_context))
+                rule.domain = "([1, '=', 1)" if rule.backend_behaviour == 'true' else "([0, '=', 1])"
             super(IrRule, self - website_rules)._force_domain()
         else:
             super(IrRule, self)._force_domain()
