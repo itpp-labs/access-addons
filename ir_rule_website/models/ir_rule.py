34add8345c9921f9c5803d1cd85879de1eb513cd
Ildar Nasyrov <nasyrov@it-projects.info>
2018-04-17 07:20:05 +0000

    [FIX] ir_rule_website: typo

--- ir_rule_website/models/ir_rule.py
+++ ir_rule_website/models/ir_rule.py
@@ -21,7 +21,7 @@ class IrRule(models.Model):
         if not eval_context.get('website_id'):
             website_rules = self.filtered(lambda r: r.backend_behaviour)
             for rule in website_rules:
-                rule.domain = "([1, '=', 1)" if rule.backend_behaviour == 'true' else "([0, '=', 1])"
+                rule.domain = "([1, '=', 1)]" if rule.backend_behaviour == 'true' else "([0, '=', 1])"
             super(IrRule, self - website_rules)._force_domain()
         else:
             super(IrRule, self)._force_domain()
