cf4b7d362ed7064467cfbed78cbb4f3970b70433
Ildar Nasyrov <nasyrov@it-projects.info>
2018-08-05 08:32:29 +0000

    :book: access_restricted

--- access_restricted/__openerp__.py
+++ access_restricted/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Restricted administration rights',
-    'version': '10.0.1.3.0',
+    'version': '10.0.1.3.1',
     'author': 'IT-Projects LLC, Ivan Yelizariev',
     "category": "Access",
     "support": "apps@it-projects.info",
