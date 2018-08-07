bec87913e55859dc6fe2f4e53453c1adf8179d86
Ildar Nasyrov <nasyrov@it-projects.info>
2018-08-07 06:23:20 +0000

    :shield: case when in a rule the `website` object references another object

--- ir_rule_website/tests/test_backend_website_rule.py
+++ ir_rule_website/tests/test_backend_website_rule.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from openerp.tests.common import TransactionCase
+from odoo.exceptions import AccessError
 
 
 class TestBackendWebsiteRule(TransactionCase):
@@ -8,6 +9,8 @@ class TestBackendWebsiteRule(TransactionCase):
 
     def setUp(self):
         super(TestBackendWebsiteRule, self).setUp()
+        self.website1 = self.env.ref('website.default_website')
+        self.website2 = self.env.ref('website.website2')
         self.user1 = self.env.ref('base.user_demo')
         self.user1.write({
             'backend_website_id': self.env.ref('website.default_website').id,
@@ -32,8 +35,41 @@ class TestBackendWebsiteRule(TransactionCase):
             'backend_website_id': self.env.ref('website.default_website').id,
         })
 
+        # case for an object references another object
+        # e.g. [('id','=', website.company_id.id)]
+        model_res_company = self.env.ref('base.model_res_company')
+        company2_partner = self.env['res.partner'].create({
+            'name': 'Partner for Comapny2',
+            'is_company': True,
+        })
+        self.company1 = self.env.ref('base.main_company')
+        self.company2 = self.env['res.company'].create({
+            'name': 'Test Company2',
+            'partner_id': company2_partner.id,
+            'parent_id': False,
+        })
+        self.website2.company_id = self.company2.id
+        self.env['ir.rule'].create({
+            'name': 'test website references company',
+            'model_id': model_res_company.id,
+            'domain_force': "[('id','=', website.company_id.id)]",
+            'backend_behaviour': 'false',
+            'groups': [(6, 0, [self.env.ref('base.group_user').id])],
+        })
+
     def test_backend_website_rule(self):
         users = self.env['res.users'].sudo(user=self.user3.id).search([])
         self.assertTrue(self.user2 not in users)
         users = self.env['res.users'].sudo(user=self.user2.id).search([])
         self.assertTrue(self.user2 in users)
+
+        # case when in a rule the `website` object references another object (`res.company` in that case)
+        companies = self.env['res.company'].sudo(user=self.user1.id).search([])
+        self.assertFalse(self.company2 in companies)
+        with self.assertRaises(AccessError):
+            self.env['res.company'].sudo(user=self.user1.id).browse(self.company2.id).name
+
+        companies = self.env['res.company'].sudo(user=self.user1.id).with_context(website_id=self.website2.id).search([])
+        self.assertTrue(self.company2 in companies)
+        name = self.env['res.company'].sudo(user=self.user1.id).with_context(website_id=self.website2.id).browse(self.company2.id).name
+        self.assertEqual(name, self.company2.name)
