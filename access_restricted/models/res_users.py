3db8b76286e3f3e8c5841298ceeb74a54131c681
Ildar Nasyrov <nasyrov@it-projects.info>
2018-08-08 11:20:25 +0000

    :ambulance: access_restricted: only allow to exclude users from groups from settings if there is no permission

--- access_restricted/models/res_users.py
+++ access_restricted/models/res_users.py
@@ -52,24 +52,25 @@ class ResGroups(models.Model):
 
     @api.multi
     def write(self, vals):
-        context = dict(self.env.context)
-        config = context.get('config')
-        # when `res.config.settings`'s `execute` method writes the `users` field to group,
-        # it is always to remove users and the `users` field is the only key in the write dict
-        users = vals.get('users')
-        if config and isinstance(config, models.Model) and \
-           users and len(vals) == 1 and all(u[0] == 3 for u in users):
-            # `isinsnance` check is a non-xmplrpc proof.
-            # allow to remove users from a group when a user uncheck group_XXX field in settings
-            # ``all(u[0] == 3 for u in users)`` is to be sure that all operations are for removing.
-            # `(3, id)` tuple removes the record from the set (the Many2many field `users`)
-            self = self.sudo()
-        if config and self.env['res.users'].has_group('access_restricted.group_allow_add_implied_from_settings'):
-            classified = config._get_classified_fields()
+        config = self.env.context.get('config')
+
+        # `isinsnance` check is a non-xmplrpc proof.
+        if config and isinstance(config, models.Model):
             implied_ids = vals.get('implied_ids')
-            if classified['group']:
-                allowed_implied = [group[2].id for group in classified['group']]
-            if implied_ids and implied_ids[0][1] in allowed_implied:
-                # allow to add implied groups from `res.config.settings`
+            classified_group = config._get_classified_fields()['group']
+            # when `res.config.settings`'s `execute` method writes the `users` field to group,
+            # it is always to remove users and the `users` field is the only key in the write dict
+            users = vals.get('users')
+            if users and len(vals) == 1 and all(u[0] == 3 for u in users) or \
+               implied_ids and implied_ids[0][1] in [group[2].id for group in classified_group] and \
+               self.env['res.users'].has_group('access_restricted.group_allow_add_implied_from_settings'):
+                # allow to remove users from a group when a user uncheck group_XXX field in settings
+                # ``all(u[0] == 3 for u in users)`` is to be sure that all operations are for removing.
+                # `(3, id)` tuple removes the record from the set (the Many2many field `users`)
+                # or
+                # allow to add implied groups from `res.config.settings` for users in special group
                 self = self.sudo()
+            else:
+                # do nothing if there is no permission to change groups from settings
+                return
         return super(ResGroups, self).write(vals)
