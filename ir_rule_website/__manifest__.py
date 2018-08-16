d128a4c959e700c847061427611bd10828e9ddb3
Ivan Yelizariev <yelizariev@it-projects.info>
2018-08-16 14:52:22 +0500

    :ambulance: wrong cache was used on working in backend

--- ir_rule_website/__manifest__.py
+++ ir_rule_website/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Access",
     # "live_test_url": "",
     "images": [],
-    "version": "11.0.1.3.0",
+    "version": "11.0.1.3.1",
     "application": False,
 
     "author": "IT-Projects LLC, Ildar Nasyrov",
