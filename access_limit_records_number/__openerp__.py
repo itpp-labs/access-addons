aaeaafa5692ab55ca56c57395527668c406286e5
Ivan Yelizariev <yelizariev@it-projects.info>
2018-09-18 17:43:50 +0500

    :arrow_up::one::two: access_limit_records_number

--- access_limit_records_number/__openerp__.py
+++ access_limit_records_number/__openerp__.py
@@ -1,10 +1,11 @@
-# -*- coding: utf-8 -*-
+# Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": """Limit number of records""",
     "summary": """e.g. allow to create maximum 10 users. Similar restriction can be applied to any table.""",
     "category": "Extra tools",
     "images": [],
-    "version": "1.0.0",
+    "version": "12.0.1.0.0",
 
     "author": "IT-Projects LLC, Pavel Romanchenko",
     "support": "apps@it-projects.info",
@@ -30,6 +31,6 @@
     "post_load": None,
     "pre_init_hook": None,
     "post_init_hook": None,
-    "installable": False,
+    "installable": True,
     "auto_install": False,
 }
