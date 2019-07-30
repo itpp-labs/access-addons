b71f963254973c42144e0c63b57a59b2cf98b9c1
ommo73 <rafailov@it-projects.info>
2019-07-30 16:06:56 +0500

    :zap: res_users_clear_access_rights New: does not clear user type when clear access rights

--- res_users_clear_access_rights/doc/changelog.rst
+++ res_users_clear_access_rights/doc/changelog.rst
@@ -1,3 +1,8 @@
+`1.1.0`
+-------
+
+- **New**: does not clear user type when clear access rights
+
 `1.0.0`
 -------
 
--- res_users_clear_access_rights/models.py
+++ res_users_clear_access_rights/models.py
@@ -13,10 +13,17 @@ class ResUsers(models.Model):
             self.env.ref('base.group_system').id,
         ]
 
+        user_types = [
+            self.env.ref('base.group_portal').id,
+            self.env.ref('base.group_public').id,
+            self.env.ref('base.group_user').id,
+        ]
+
         groups_id = []
         for g in self.groups_id:
-            if self.env.uid == self.id and g.id in admin_groups:
+            if self.env.uid == self.id and g.id in admin_groups or g.id in user_types:
                 # don't allow for Administrator to clear his admin rights
+                # don't clear user type
                 continue
             groups_id.append((3, g.id))
         self.write({'groups_id': groups_id})
