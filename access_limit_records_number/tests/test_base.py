fa978aa7bf79f2555be07800ce49aa92f1820568
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-12 08:53:10 +0000

    :green_heart: fix old imports

--- access_limit_records_number/tests/test_base.py
+++ access_limit_records_number/tests/test_base.py
@@ -1,6 +1,6 @@
 # Copyright 2018 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from openerp.tests import common
+from odoo.tests import common
 
 
 @common.tagged("post_install", "-at_install")
