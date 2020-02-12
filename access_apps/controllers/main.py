1a2c97a5c9658487a2abd1ef4c65dbc43a8ca02d
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-12 08:52:14 +0000

    :green_heart: pre-commit: auto cleanups

--- access_apps/controllers/main.py
+++ access_apps/controllers/main.py
@@ -1,6 +1,7 @@
 from odoo import SUPERUSER_ID, http
-from odoo.addons.web_settings_dashboard.controllers.main import WebSettingsDashboard
 from odoo.http import request
+
+from odoo.addons.web_settings_dashboard.controllers.main import WebSettingsDashboard
 
 
 class WebSettingsDashboardCustom(WebSettingsDashboard):
--- access_base/models.py
+++ access_base/models.py
@@ -1,8 +1,10 @@
 from lxml import etree
 from lxml.builder import E
+
 from odoo import api, fields, models
-from odoo.addons.base.res.res_users import name_boolean_group, name_selection_groups
 from odoo.tools.translate import _
+
+from odoo.addons.base.res.res_users import name_boolean_group, name_selection_groups
 
 
 class GroupsView(models.Model):
