8128d1c07a83a0593479ec0f86881ed1f7d187b2
Ivan Yelizariev <yelizariev@it-projects.info>
2020-02-17 13:47:47 +0500

    :book: Distribute under MIT license
    If you made a contribution and don't agree with this change please open an issue

--- ir_rule_protected/__manifest__.py
+++ ir_rule_protected/__manifest__.py
@@ -5,7 +5,7 @@
     "category": "Access",
     "support": "apps@it-projects.info",
     "website": "https://twitter.com/yelizariev",
-    "license": "LGPL-3",
+    "license": "Other OSI approved licence",  # MIT
     "depends": [],
     "data": ["views.xml"],
     "installable": True,
