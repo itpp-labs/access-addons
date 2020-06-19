886f03a130ac3dbf6e19eab5c68030ada0ccf24b
Eugene Molotov <molotov@it-projects.info>
2020-06-19 17:12:43 +0500

    :tada: :one::three: database_limit_size

---
+++ database_limit_size/__manifest__.py
@@ -0,0 +1,39 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License MIT (https://opensource.org/licenses/MIT).
+
+{
+    "name": """Database Limit Size""",
+    "summary": """This module allows blocking backend access when database limit is exceeded""",
+    "category": "Extra Tools",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=13.0",
+    "images": [],
+    "version": "13.0.1.0.0",
+    "application": False,
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "apps@it-projects.info",
+    "website": "https://apps.odoo.com/apps/modules/13.0/database_limit_size/",
+    "license": "Other OSI approved licence",  # MIT
+    # "price": 9.00,
+    # "currency": "EUR",
+    "depends": ["database_block"],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [],
+    "demo": [],
+    "qweb": [],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+    "auto_install": False,
+    "installable": True,
+    # "demo_title": "Database Limit Size",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "This module allows to block user to backend when database limit is exceed",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
