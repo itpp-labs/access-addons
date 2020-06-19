e9e5a5a7701210b31cd199ae8fdff40ada118957
Eugene Molotov <molotov@it-projects.info>
2020-06-19 14:53:25 +0500

    :tada: :one::three: database_block

---
+++ database_block/__manifest__.py
@@ -0,0 +1,39 @@
+# Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
+# License MIT (https://opensource.org/licenses/MIT).
+
+{
+    "name": """Block backend UI""",
+    "summary": """This technical module allows blocking backend access and display the message""",
+    "category": "Extra Tools",
+    # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=13.0",
+    "images": [],
+    "version": "13.0.1.0.0",
+    "application": False,
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "apps@it-projects.info",
+    "website": "https://apps.odoo.com/apps/modules/13.0/database_block/",
+    "license": "Other OSI approved licence",  # MIT
+    # "price": 9.00,
+    # "currency": "EUR",
+    "depends": ["web"],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": ["views/assets.xml"],
+    "demo": [],
+    "qweb": ["static/src/xml/apps.xml"],
+    "post_load": None,
+    "pre_init_hook": None,
+    "post_init_hook": None,
+    "uninstall_hook": None,
+    "auto_install": False,
+    "installable": True,
+    # "demo_title": "Database Block",
+    # "demo_addons": [
+    # ],
+    # "demo_addons_hidden": [
+    # ],
+    # "demo_url": "DEMO-URL",
+    # "demo_summary": "This module allows to block user to backend with specified reason",
+    # "demo_images": [
+    #    "images/MAIN_IMAGE",
+    # ]
+}
