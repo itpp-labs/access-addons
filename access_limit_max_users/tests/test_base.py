0d9d5a4c13ad5b1ec8770df4f832f14af198d6e0
Eugene Molotov <molotov@it-projects.info>
2020-06-23 12:28:08 +0500

    :shield: access_limit_max_users: fix tests to correctly handle limit reached exception instead of other exception

--- access_limit_max_users/tests/test_base.py
+++ access_limit_max_users/tests/test_base.py
@@ -1,5 +1,6 @@
 # Copyright 2020 Eugene Molotov <https://it-projects.info/team/em230418>
 # License MIT (https://opensource.org/licenses/MIT).
+from odoo import exceptions
 from odoo.tests import common
 
 
@@ -9,16 +10,18 @@ class TestBase(common.TransactionCase):
         admin_user = self.env.ref("base.user_admin")
 
         users_count = self.env["res.users"].sudo().search_count([])
-        max_users_allowed = self.env.ref(
-            "access_limit_max_users.max_users_limit"
-        ).max_records
+        rule_record = self.env.ref("access_limit_max_users.max_users_limit")
+        rule_record.max_records = users_count + 1
+        max_users_allowed = rule_record.max_records
 
         Users = self.env["res.users"].sudo(admin_user)
 
         while users_count < max_users_allowed:
-            Users.create({"name": "test_max_users_{}".format(users_count)})
+            login = "test_max_users_{}".format(users_count)
+            Users.create({"name": login, "login": login})
             users_count += 1
 
         # limit is reached
-        with self.assertRaises(Exception):
-            Users.create({"name": "test_max_users_{}".format(users_count)})
+        with self.assertRaises(exceptions.UserError):
+            login = "test_max_users_{}".format(users_count)
+            Users.create({"name": login, "login": login})
