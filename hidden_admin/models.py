421fb60b25bfea67ccf0309f3cf7385dafafc350
Mario Nuñez <marionumza@gmail.com>
2020-08-22 19:39:20 -0300

    [MIG-12] hidden_admin

---
+++ hidden_admin/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import SUPERUSER_ID, api, models
+
+
+class Channel(models.Model):
+    _inherit = "mail.channel"
+
+    @api.multi
+    def channel_fetch_listeners(self, uuid):
+        admin_id = self.env["res.users"].sudo().browse(SUPERUSER_ID).partner_id.id
+        res = super(Channel, self).channel_fetch_listeners(uuid)
+        return [p for p in res if p.get("id") != admin_id]
