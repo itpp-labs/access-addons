97696457cc367c6ca9f53cd01ead53107b00281b
abderrahmen.khalledi <abderrahmen.khalledi@sirum.de>
2020-11-02 13:07:13 +0100

    [FIX]: using res.user field in widget domain of base automation

--- access_settings_menu/models.py
+++ access_settings_menu/models.py
@@ -1,3 +1,4 @@
+from inspect import signature
 from odoo import api, models
 
 
@@ -7,4 +8,7 @@ class ResUsers(models.Model):
     @api.model
     def fields_get(self, *args, **kwargs):
         # switch to superuser to get access to virtual fields
+        # clean kwargs before call the function
+        sig = signature(super(ResUsers, self.sudo()).fields_get)
+        kwargs = {key: kwargs[key] for key in kwargs.keys() if sig.parameters.get(key)}
         return super(ResUsers, self.sudo()).fields_get(*args, **kwargs)
