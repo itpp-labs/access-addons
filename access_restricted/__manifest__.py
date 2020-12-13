cbc8a9d1a40d599882e676746e97006112e15617
Mitchell Admin <itpp-bot@users.noreply.github.com>
2020-12-13 17:37:39 +0000

    :tada::one::three: access_restricted previous commits history: https://github.com/itpp-labs/access-addons/commits/13.0/ir_rule_protected
    > Made via .github/workflows/DINAR-PORT.yml

---
+++ access_restricted/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Restricted administration rights",
+    "version": "13.0.1.3.5",
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "category": "Access",
+    "images": ["images/icon.png"],
+    "support": "apps@itpp.dev",
+    "website": "https://apps.odoo.com/apps/modules/12.0/access_restricted/",
+    "license": "Other OSI approved licence",  # MIT
+    "price": 30.00,
+    "currency": "EUR",
+    "depends": ["ir_rule_protected"],
+    "data": ["security/access_restricted_security.xml"],
+    "installable": True,
+}
