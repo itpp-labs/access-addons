f11ba2d871d49f3de7142c46329b34e697ce1c67
Ivan Yelizariev <iel@odoo.com>
2020-12-26 17:46:14 +0100

    :heart: Buy a coffee for us, please :pray: ko-fi.com/itprojectsllc

--- access_restricted/__openerp__.py
+++ access_restricted/__openerp__.py
@@ -6,8 +6,6 @@
     "category": "Access",
     'website': 'https://twitter.com/yelizariev',
     "license": "Other OSI approved licence",  # MIT
-    'price': 30.00,
-    'currency': 'EUR',
     'depends': ['ir_rule_protected', 'share'],
     'data': [
         'security.xml',
