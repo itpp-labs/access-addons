140545f6c43b9faaa243c714171e0ae9e3fcaa01
Eugene Molotov <molotov@it-projects.info>
2021-01-25 15:22:25 +0500

    :ambulance: access_limit_max_users: fixed impossibility to successfully edit any user

--- access_limit_max_users/__manifest__.py
+++ access_limit_max_users/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "Hidden",
     # "live_test_url": "http://apps.it-projects.info/shop/product/DEMO-URL?version=12.0",
     "images": [],
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "application": False,
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "help@itpp.dev",
