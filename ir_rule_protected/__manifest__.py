3849132995edae82b9a02c621c1b390717516f19
Ivan Yelizariev <iel@odoo.com>
2021-06-27 14:35:57 +0200

    :book: update banners

--- ir_rule_protected/__manifest__.py
+++ ir_rule_protected/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "14.0.1.0.0",
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "category": "Access",
-    "images": ["images/icon.png"],
+    "images": ["images/banner.jpg"],
     "support": "apps@itpp.dev",
     "website": "https://www.odoo.com/apps/modules/14.0/ir_rule_protected/",
     "license": "Other OSI approved licence",  # MIT
