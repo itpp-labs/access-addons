a72a6aafdb676ebc831b605ac889f402758706f9
Ivan Yelizariev <iel@odoo.com>
2021-06-27 16:22:25 +0200

    :book: fix summary, icon, category

--- ir_rule_protected/__manifest__.py
+++ ir_rule_protected/__manifest__.py
@@ -1,8 +1,9 @@
 {
     "name": "Protect ir.rule records",
+    "summary": "Non-superuser admins cannot change protected ir.rule or uninstall this module",
     "version": "14.0.1.0.0",
     "author": "IT-Projects LLC, Ivan Yelizariev",
-    "category": "Access",
+    "category": "Extra Tools",
     "images": ["images/banner.jpg"],
     "support": "apps@itpp.dev",
     "website": "https://www.odoo.com/apps/modules/14.0/ir_rule_protected/",
