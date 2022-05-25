30602007c6819b9afe8be29a768c91f4b333fd61
Mitchell Admin <itpp-bot@users.noreply.github.com>
2022-05-25 08:56:48 +0000

    :tada::one::four: access_restricted previous commits history: https://github.com/itpp-labs/access-addons/commits/14.0/access_restricted > Made via .github/workflows/DINAR-PORT.yml

---
+++ access_restricted/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Restricted administration rights",
+    "summary": "Apply strict restriction on what an admin can do",
+    "version": "14.0.1.3.5",
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "category": "Extra Tools",
+    "images": ["images/banner.jpg"],
+    "support": "apps@itpp.dev",
+    "website": "https://twitter.com/OdooFree",
+    "license": "Other OSI approved licence",  # MIT
+    "currency": "EUR",
+    "depends": ["ir_rule_protected"],
+    "data": ["security/access_restricted_security.xml"],
+    "installable": True,
+}
