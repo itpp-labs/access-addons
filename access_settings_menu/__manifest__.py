1336d75b5f968ff21fda05e266347685982d03a0
Ivan Yelizariev <iel@odoo.com>
2022-05-25 11:40:26 +0200

    :sparkles: replace dep to access_restricted

--- access_settings_menu/__manifest__.py
+++ access_settings_menu/__manifest__.py
@@ -1,14 +1,14 @@
 {
     "name": "Show settings menu for non-admin",
     "summary": """Allows to make Settings menu visible for non-admins""",
-    "version": "15.0.1.0.2",
+    "version": "15.0.2.0.0",
     "author": "IT-Projects LLC, Ivan Yelizariev",
     "category": "Extra Tools",
     "images": ["images/banner.png"],
     "support": "apps@itpp.dev",
     "website": "https://twitter.com/OdooFree",
     "license": "Other OSI approved licence",  # MIT
-    "depends": ["access_apps"],
+    "depends": ["access_restricted"],
     "data": ["security/access_settings_menu_security.xml"],
     "demo": ["security/access_settings_menu_security_demo.xml"],
     "installable": True,
--- access_settings_menu/doc/changelog.rst
+++ access_settings_menu/doc/changelog.rst
@@ -1,3 +1,8 @@
+`2.0.0`
+-------
+
+- Improvement: replace `access_apps` dependency with `access_restricted` since only the latter one is needed
+
 `1.0.2`
 -------
 
