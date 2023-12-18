35bb4e5f7813c9b24600d798966ce9689bf8d9ab
Ivan Kropotkin <yelizariev@itpp.dev>
2023-12-18 10:44:30 +0100

    :ambulance: access_apps: fix uninstall_hook

--- access_apps/hooks.py
+++ access_apps/hooks.py
@@ -1,10 +1,6 @@
-# Copyright 2021 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
+# Copyright 2021,2023 Ivan Yelizariev <https://it-projects.info/team/yelizariev>
 # License MIT (https://opensource.org/licenses/MIT).
-from odoo import SUPERUSER_ID, api
-
-
-def uninstall_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def uninstall_hook(env):
     access = env.ref(
         "base.access_ir_module_module_group_user", raise_if_not_found=False
     )
